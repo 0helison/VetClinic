8643dff97c45eb0dac26ac7070d5635e441f8b19
helissonpontes69@gmail.com <helissonpontes69@gmail.com>
2024-05-09 12:21:28 -0300

    feat: created validation of requests for pets and tutors

--- src/routers/PetRouters.js
+++ src/routers/PetRouters.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const PetController = require('../app/controller/PetController');
+const petCreateValidation = require('../app/middleware/petValidation');
+const validate = require('../app/middleware/handleValidation');
 
-router.post('/pet/:tutorId', PetController.createPet);
-router.put('/pet/:petId/tutor/:tutorId', PetController.updatePet)
+router.post('/pet/:tutorId', petCreateValidation(), validate, PetController.createPet);
+router.put('/pet/:petId/tutor/:tutorId', petCreateValidation(), validate, PetController.updatePet)
 router.delete('/pet/:petId/tutor/:tutorId', PetController.removePet)
 
 module.exports = router;  
