6a440b7c245b8690c806f9d9bc13e57fd4afdd4f
helissonpontes69@gmail.com <helissonpontes69@gmail.com>
2024-05-08 22:22:39 -0300

    chore: design changes that do not affect the system

--- src/app.js
+++ src/app.js
@@ -1,25 +1,17 @@
 const express = require("express");
 const app = express();
 require('dotenv').config();
-const db = require("./db/connection");
+const syncDB = require("./config/db");
 const TutorRouters = require("./routers/TutorRouters");
 const PetRouters = require("./routers/PetRouters");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/vetclinic', TutorRouters); 
 app.use('/vetclinic', PetRouters);
 
-db
-  .authenticate()
-  .then(() => {
-    console.log("Conectou ao banco com sucesso");
-
-    app.listen(PORT, function() {
-      console.log(`O Express está rodando na porta ${PORT}`);
-    });
-  })
-  .catch(err => {
-    console.log("Ocorreu um erro ao conectar", err);
-  });
+app.listen(PORT, async () => {
+  await syncDB();
+  console.log(`O Express está rodando na porta ${PORT}`);
+});
