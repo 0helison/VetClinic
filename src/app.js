2f8e07d27be0309db5485526aa59cfcdfd9bdde3
helissonpontes69@gmail.com <helissonpontes69@gmail.com>
2024-05-12 18:51:02 -0300

    Revert "fixed: remove Swagger from the application"
    This reverts commit 167cf5580da008f290eba9fa2b3766bee9becd9e.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const syncDB = require('./config/db');
+const swaggerUI = require('swagger-ui-express')
+const swaggerDocument = require('../swagger.json')
 const TutorRouters = require('./routers/TutorRouters');
 const PetRouters = require('./routers/PetRouters');
 
@@ -10,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/vetclinic', TutorRouters); 
 app.use('/vetclinic', PetRouters);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(PORT, async () => {
   await syncDB();
